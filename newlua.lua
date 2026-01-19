f816cbeb49e7206267e96b54470a744e38c17242
larega6-sketch <larega6@gmail.com>
2026-01-19 13:39:26 +0300

    Update newlua.lua

--- newlua.lua
+++ newlua.lua
@@ -1,566 +1,249 @@
---[[
-                Example Compkiller UI
-        
-        Author: 4lpaca
-        
-        Press Left Alt to open / close
-]]
-
 local Compkiller = loadstring(game:HttpGet("https://raw.githubusercontent.com/4lpaca-pin/CompKiller/refs/heads/main/src/source.luau"))();
 
-
--- Create Notification --
-local Notifier = Compkiller.newNotify();
-
--- Create Config Mamager --
 local ConfigManager = Compkiller:ConfigManager({
-        Directory = "Compkiller-UI",
-        Config = "Example-Configs"
+	Directory = "Compkiller-UI",
+	Config = "AimConfigs",
 });
 
--- Loading UI (Icon <string> , Duration <number>) --
-Compkiller:Loader("rbxassetid://120245531583106" , 1.5).yield();
-
--- Creating Window --
-local MenuKey = "LeftAlt";
+Compkiller:Loader("rbxassetid://120245531583106", 2.5).yield();
 
 local Window = Compkiller.new({
-        Name = "COMPKILLER",
-        Keybind = MenuKey,
-        Logo = "rbxassetid://120245531583106",
-        Scale = Compkiller.Scale.Window, -- Leave blank if you want automatic scale [PC, Mobile].
-        TextSize = 15,
+	Name = "RAGE HUB",
+	Keybind = "RightControl",
+	Logo = "rbxassetid://120245531583106",
+	TextSize = 15,
 });
-
--- User Setting --
-
-local UserSettings = Window.UserSettings:Create();
-
-UserSettings:AddColorPicker({
-        Name = "Menu Color",
-        Default = Compkiller.Colors.Highlight,
-        Callback = function(f)
-                Compkiller.Colors.Highlight = f;
-
-                Compkiller:RefreshCurrentColor();
-        end,
-});
-
-UserSettings:AddKeybind({
-        Name = "Menu Key",
-        Default = MenuKey,
-        Callback = function(f)
-                MenuKey = f;
-
-                Window:SetMenuKey(MenuKey)
-        end,
-});
-
-UserSettings:AddDropdown({
-        Name = "Menu Language",
-        Values = {"English","Russian","Chinese"},
-        Default = "English",
-        Callback = print
-});
-
-UserSettings:AddDropdown({
-        Name = "Menu Theme",
-        Values = {
-                "Default",
-                "Dark Green",
-                "Dark Blue",
-                "Purple Rose",
-                "Skeet"
-        },
-
-        Default = "Default",
-
-        Callback = function(f)
-                Compkiller:SetTheme(f)
-        end,
-});
-
-UserSettings:AddDropdown({
-        Name = "Visible Widgets",
-        Values = {"Watermark","Keybinds","Double Tab"},
-        Multi = true,
-        Default = {"Watermark"},
-        Callback = function(f)
-                print(f)
-        end,
-});
-
--- Notification --
-
-Notifier.new({
-        Title = "Notification",
-        Content = "Thank you for use this script!",
-        Duration = 10,
-        Icon = "rbxassetid://120245531583106"
-});
-
--- Watermark --
-local Watermark = Window:Watermark();
-
-Watermark:AddText({
-        Icon = "user",
-        Text = "4lpaca",
-});
-
-Watermark:AddText({
-        Icon = "clock",
-        Text = Compkiller:GetDate(),
-});
-
-local Time = Watermark:AddText({
-        Icon = "timer",
-        Text = "TIME",
-});
-
-task.spawn(function()
-        while true do task.wait()
-                Time:SetText(Compkiller:GetTimeNow());
-        end
-end)
-
-Watermark:AddText({
-        Icon = "server",
-        Text = Compkiller.Version,
-});
-
--- Creating Tab Category --
-Window:DrawCategory({
-        Name = "Example"
-});
-
--- Creating Tab --
-local NormalTab = Window:DrawTab({
-        Name = "Example Tab",
-        Icon = "apple",
-        EnableScrolling = true
-});
-
--- Creating Section --
-local NormalSection = NormalTab:DrawSection({
-        Name = "Section",
-        Position = 'left'        
-});
-
-local Toggle = NormalSection:AddToggle({
-        Name = "Toggle",
-        Flag = "Toggle_Example", -- Leave it blank will not save to config
-        Default = false,
-        Callback = print,
-});
-
--- Add Keybind To Toggle --
-local Keybind = Toggle.Link:AddKeybind({
-        Default = "E",
-        Flag = "Option_Keybind",
-        Callback = print
-});
-
--- Helper --
-Toggle.Link:AddHelper({
-        Text = "Very cool toggle!"
-})
-
--- Add Option To Toggle --
-local Toggle2 = NormalSection:AddToggle({
-        Name = "Toggle",
-        Flag = "Toggle_Example2", -- Leave it blank will not save to config
-        Default = false,
-        Callback = print,
-});
-
-local Option = Toggle2.Link:AddOption()
-
-Option:AddToggle({
-        Name= "Example",
-        Flag = "Toggle_Example3",
-        Callback = print
-});
-
-do
-        local Toggle2 = NormalSection:AddToggle({
-                Name = "Risky Feature",
-                Flag = "Toggle_Example5", -- Leave it blank will not save to config
-                Default = false,
-                Risky = true,
-                Callback = print,
-        });
-
-        local Option = Toggle2.Link:AddOption()
-
-        Option:AddToggle({
-                Risky = true,
-                Name= "Risky Feature",
-                Flag = "Toggle_Example6",
-                Callback = print
-        });
-end
-
-NormalSection:AddKeybind({
-        Name = "Keybind",
-        Default = "LeftAlt",
-        Flag = "Keybind_Example",
-        Callback = print,
-});
-
-NormalSection:AddSlider({
-        Name = "Slider",
-        Min = 0,
-        Max = 100,
-        Default = 50,
-        Round = 0,
-        Flag = "Slider_Example",
-        Callback = print
-});
-
-NormalSection:AddColorPicker({
-        Name = "ColorPicker",
-        Default = Color3.fromRGB(0, 255, 140),
-        Flag = "Color_Picker_Example",
-        Callback = print
-})
-
-NormalSection:AddDropdown({
-        Name = "Single Dropdown",
-        Default = "Head",
-        Flag = "Single_Dropdown",
-        Values = {"Head","Body","Arms","Legs"},
-        Callback = print
-})
-
-NormalSection:AddDropdown({
-        Name = "Multi Dropdown",
-        Default = {"Head"},
-        Multi = true,
-        Flag = "Multi_Dropdown",
-        Values = {"Head","Body","Arms","Legs"},
-        Callback = print
-})
-
-NormalSection:AddButton({
-        Name = "Button",
-        Callback = function()
-                print('PRINT!')
-        end,
-})
-
-NormalSection:AddParagraph({
-        Title = "Paragraph",
-        Content = "Very cool paragraph\nAll element in this scrtion\nwill be saved to the config!"
-})
-
-NormalSection:AddTextBox({
-        Name = "Textbox",
-        Placeholder = "Placeholder",
-        Default = "Hello, World",
-        Callback = print
-})
-
-local DrawElements = function(Tab,Position)
-        do
-                local NormalSectionRight = Tab:DrawSection({
-                        Name = "Section",
-                        Position = Position
-                });
-
-                local Toggle = NormalSectionRight:AddToggle({
-                        Name = "Toggle",
-                        Default = false,
-                        Callback = print,
-                });
-
-                -- Add Keybind To Toggle --
-                local Keybind = Toggle.Link:AddKeybind({
-                        Default = "E",
-                        Callback = print
-                });
-
-                -- Add Option To Toggle --
-                local Toggle2 = NormalSectionRight:AddToggle({
-                        Name = "Toggle",
-                        Default = false,
-                        Callback = print,
-                });
-
-                local Option = Toggle2.Link:AddOption()
-
-                Option:AddToggle({
-                        Name= "Example",
-                        Callback = print
-                });
-
-                NormalSectionRight:AddKeybind({
-                        Name = "Keybind",
-                        Default = "LeftAlt",
-                        Callback = print,
-                });
-
-                NormalSectionRight:AddSlider({
-                        Name = "Slider",
-                        Min = 0,
-                        Max = 100,
-                        Default = 50,
-                        Round = 0,
-                        Callback = print
-                });
-
-                NormalSectionRight:AddColorPicker({
-                        Name = "ColorPicker",
-                        Default = Color3.fromRGB(0, 255, 140),
-                        Callback = print
-                })
-
-                NormalSectionRight:AddDropdown({
-                        Name = "Single Dropdown",
-                        Default = "Head",
-                        Values = {"Head","Body","Arms","Legs"},
-                        Callback = print
-                })
-
-                NormalSectionRight:AddDropdown({
-                        Name = "Multi Dropdown",
-                        Default = {"Head"},
-                        Multi = true,
-                        Values = {"Head","Body","Arms","Legs"},
-                        Callback = print
-                })
-
-                NormalSectionRight:AddButton({
-                        Name = "Button",
-                        Callback = function()
-                                print('PRINT!')
-                        end,
-                })
-
-                NormalSectionRight:AddParagraph({
-                        Title = "Paragraph",
-                        Content = "Very cool paragraph\nAll elements in this section\nwill not be save to the config"
-                })
-        end;
-end;
-
-DrawElements(NormalTab,'right')
-
--- Single Tab --
-local SingleTab = Window:DrawTab({
-        Name = "Single Tab",
-        Icon = "banana",
-        Type = "Single"
-});
-
-DrawElements(SingleTab,'left')
-
--- Container Tab --
-local ContainerTab = Window:DrawContainerTab({
-        Name = "Extract Tabs",
-        Icon = "contact",
-});
-
-local ExtractTab = ContainerTab:DrawTab({
-        Name = "Tab 1",
-        Type = "Double"
-});
-
-local SingleExtractTab = ContainerTab:DrawTab({
-        Name = "Tab 2",
-        Type = "Single",
-        EnableScrolling = true, -- this will make tab can scrolling (recommend)
-});
-
-DrawElements(ExtractTab,"left");
-DrawElements(ExtractTab,"right");
-
-DrawElements(SingleExtractTab,"left");
-DrawElements(SingleExtractTab,"right");
 
 Window:DrawCategory({
-        Name = "Misc"
+	Name = "Main"
 });
 
-local SettingTab = Window:DrawTab({
-        Icon = "settings-3",
-        Name = "Settings",
-        Type = "Single",
-        EnableScrolling = true
+-- RAGE BOT TAB --
+local RageTab = Window:DrawTab({
+	Name = "Rage Bot",
+	Icon = "target",
 });
 
-local ThemeTab = Window:DrawTab({
-        Icon = "paintbrush",
-        Name = "Themes",
-        Type = "Single"
+local AimSection = RageTab:DrawSection({
+	Name = "Aimbot",
+	Position = 'left'	
 });
 
-local Settings = SettingTab:DrawSection({
-        Name = "UI Settings",
+AimSection:AddToggle({
+	Name = "Enable Aimbot",
+	Flag = "aimbot_enable",
+	Default = false,
+	Callback = function(state)
+		print("Aimbot:", state)
+	end,
 });
 
-Settings:AddToggle({
-        Name = "Alway Show Frame",
-        Default = false,
-        Callback = function(v)
-                Window.AlwayShowTab = v;
-        end,
+AimSection:AddKeybind({
+	Name = "Aimbot Key",
+	Default = "MouseButton2",
+	Flag = "aimbot_key",
+	Callback = print,
 });
 
-Settings:AddColorPicker({
-        Name = "Highlight",
-        Default = Compkiller.Colors.Highlight,
-        Callback = function(v)
-                Compkiller.Colors.Highlight = v;
-                Compkiller:RefreshCurrentColor();
-        end,
+AimSection:AddSlider({
+	Name = "Field of View",
+	Min = 0,
+	Max = 360,
+	Default = 90,
+	Round = 0,
+	Flag = "aimbot_fov",
+	Callback = print
 });
 
-Settings:AddColorPicker({
-        Name = "Toggle Color",
-        Default = Compkiller.Colors.Toggle,
-        Callback = function(v)
-                Compkiller.Colors.Toggle = v;
-
-                Compkiller:RefreshCurrentColor(v);
-        end,
+AimSection:AddDropdown({
+	Name = "Hitbox Priority",
+	Default = "Head",
+	Flag = "aimbot_hitbox",
+	Values = {"Head", "Body", "Random"},
+	Callback = print
 });
 
-Settings:AddColorPicker({
-        Name = "Drop Color",
-        Default = Compkiller.Colors.DropColor,
-        Callback = function(v)
-                Compkiller.Colors.DropColor = v;
-
-                Compkiller:RefreshCurrentColor(v);
-        end,
+local WeaponSection = RageTab:DrawSection({
+	Name = "Weapon",
+	Position = 'right'	
 });
 
-Settings:AddColorPicker({
-        Name = "Risky",
-        Default = Compkiller.Colors.Risky,
-        Callback = function(v)
-                Compkiller.Colors.Risky = v;
-
-                Compkiller:RefreshCurrentColor(v);
-        end,
+WeaponSection:AddToggle({
+	Name = "Silent Aim",
+	Flag = "silent_aim",
+	Default = false,
+	Callback = print,
 });
 
-Settings:AddColorPicker({
-        Name = "Mouse Enter",
-        Default = Compkiller.Colors.MouseEnter,
-        Callback = function(v)
-                Compkiller.Colors.MouseEnter = v;
-
-                Compkiller:RefreshCurrentColor(v);
-        end,
+WeaponSection:AddToggle({
+	Name = "Auto Shoot",
+	Flag = "auto_shoot",
+	Default = false,
+	Callback = print,
 });
 
-Settings:AddColorPicker({
-        Name = "Block Color",
-        Default = Compkiller.Colors.BlockColor,
-        Callback = function(v)
-                Compkiller.Colors.BlockColor = v;
-
-                Compkiller:RefreshCurrentColor(v);
-        end,
+WeaponSection:AddSlider({
+	Name = "Hit Chance %",
+	Min = 0,
+	Max = 100,
+	Default = 80,
+	Round = 0,
+	Flag = "hit_chance",
+	Callback = print
 });
 
-Settings:AddColorPicker({
-        Name = "Background Color",
-        Default = Compkiller.Colors.BGDBColor,
-        Callback = function(v)
-                Compkiller.Colors.BGDBColor = v;
-
-                Compkiller:RefreshCurrentColor(v);
-        end,
+-- ANTI-AIM TAB --
+local AATab = Window:DrawTab({
+	Name = "Anti-Aim",
+	Icon = "shield",
+	Type = "Single",
 });
 
-Settings:AddColorPicker({
-        Name = "Block Background Color",
-        Default = Compkiller.Colors.BlockBackground,
-        Callback = function(v)
-                Compkiller.Colors.BlockBackground = v;
-
-                Compkiller:RefreshCurrentColor(v);
-        end,
+local AASection = AATab:DrawSection({
+	Name = "Anti-Aim Settings",
+	Position = 'left'	
 });
 
-Settings:AddColorPicker({
-        Name = "Stroke Color",
-        Default = Compkiller.Colors.StrokeColor,
-        Callback = function(v)
-                Compkiller.Colors.StrokeColor = v;
-
-                Compkiller:RefreshCurrentColor(v);
-        end,
+AASection:AddToggle({
+	Name = "Enable Anti-Aim",
+	Flag = "aa_enable",
+	Default = false,
+	Callback = print,
 });
 
-Settings:AddColorPicker({
-        Name = "High Stroke Color",
-        Default = Compkiller.Colors.HighStrokeColor,
-        Callback = function(v)
-                Compkiller.Colors.HighStrokeColor = v;
-
-                Compkiller:RefreshCurrentColor(v);
-        end,
+AASection:AddDropdown({
+	Name = "Pitch",
+	Default = "Default",
+	Flag = "aa_pitch",
+	Values = {"Default", "Down", "Up", "Random"},
+	Callback = print
 });
 
-Settings:AddColorPicker({
-        Name = "Switch Color",
-        Default = Compkiller.Colors.SwitchColor,
-        Callback = function(v)
-                Compkiller.Colors.SwitchColor = v;
-
-                Compkiller:RefreshCurrentColor(v);
-        end,
+AASection:AddDropdown({
+	Name = "Yaw",
+	Default = "Default",
+	Flag = "aa_yaw",
+	Values = {"Default", "Backwards", "Spin", "Jitter"},
+	Callback = print
 });
 
-Settings:AddColorPicker({
-        Name = "Line Color",
-        Default = Compkiller.Colors.LineColor,
-        Callback = function(v)
-                Compkiller.Colors.LineColor = v;
-
-                Compkiller:RefreshCurrentColor(v);
-        end,
+AASection:AddSlider({
+	Name = "Jitter Range",
+	Min = 0,
+	Max = 180,
+	Default = 45,
+	Round = 0,
+	Flag = "aa_jitter_range",
+	Callback = print
 });
 
-Settings:AddButton({
-        Name = "Get Theme",
-        Callback = function()
-                print(Compkiller:GetTheme())
-
-                Notifier.new({
-                        Title = "Notification",
-                        Content = "Copied Them Color to your clipboard",
-                        Duration = 5,
-                        Icon = "rbxassetid://120245531583106"
-                });
-        end,
+local FakeLagSection = AATab:DrawSection({
+	Name = "Fake Lag",
+	Position = 'right'	
 });
 
-ThemeTab:DrawSection({
-        Name = "UI Themes"
-}):AddDropdown({
-        Name = "Select Theme",
-        Default = "Default",
-        Values = {
-                "Default",
-                "Dark Green",
-                "Dark Blue",
-                "Purple Rose",
-                "Skeet"
-        },
-        Callback = function(v)
-                Compkiller:SetTheme(v)
-        end,
-})
+FakeLagSection:AddToggle({
+	Name = "Enable Fake Lag",
+	Flag = "fakelag_enable",
+	Default = false,
+	Callback = print,
+});
 
--- Creating Config Tab --
+FakeLagSection:AddSlider({
+	Name = "Lag Amount",
+	Min = 0,
+	Max = 100,
+	Default = 30,
+	Round = 0,
+	Flag = "fakelag_amount",
+	Callback = print
+});
+
+FakeLagSection:AddDropdown({
+	Name = "Lag Type",
+	Default = "Adaptive",
+	Flag = "fakelag_type",
+	Values = {"Adaptive", "Static", "Random"},
+	Callback = print
+});
+
+-- VISUAL TAB --
+local VisualTab = Window:DrawTab({
+	Name = "Visual",
+	Icon = "eye",
+});
+
+local ESPsection = VisualTab:DrawSection({
+	Name = "ESP",
+	Position = 'left'	
+});
+
+ESPsection:AddToggle({
+	Name = "Enable ESP",
+	Flag = "esp_enable",
+	Default = true,
+	Callback = print,
+});
+
+ESPsection:AddColorPicker({
+	Name = "Box Color",
+	Default = Color3.fromRGB(0, 255, 140),
+	Flag = "esp_box_color",
+	Callback = print
+});
+
+ESPsection:AddToggle({
+	Name = "Show Name",
+	Flag = "esp_name",
+	Default = true,
+	Callback = print,
+});
+
+ESPsection:AddToggle({
+	Name = "Show Health",
+	Flag = "esp_health",
+	Default = true,
+	Callback = print,
+});
+
+local MiscSection = VisualTab:DrawSection({
+	Name = "Visual Effects",
+	Position = 'right'	
+});
+
+MiscSection:AddToggle({
+	Name = "Chams",
+	Flag = "chams_enable",
+	Default = false,
+	Callback = print,
+});
+
+MiscSection:AddColorPicker({
+	Name = "Chams Color",
+	Default = Color3.fromRGB(255, 0, 0),
+	Flag = "chams_color",
+	Callback = print
+});
+
+MiscSection:AddToggle({
+	Name = "No Flash",
+	Flag = "no_flash",
+	Default = true,
+	Callback = print,
+});
+
+MiscSection:AddToggle({
+	Name = "No Smoke",
+	Flag = "no_smoke",
+	Default = false,
+	Callback = print,
+});
+
+-- CONFIG TAB --
 local ConfigUI = Window:DrawConfig({
-        Name = "Config",
-        Icon = "folder",
-        Config = ConfigManager
+	Name = "Config",
+	Icon = "folder",
+	Config = ConfigManager,
 });
 
 ConfigUI:Init();
